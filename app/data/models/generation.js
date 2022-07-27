7906f586008425807142cd19e3751243ed3ffb24
Simon Dunn <simondunn90@gmail.com>
2022-07-27 08:22:51 +0100

    change column name to dateGenerated for clarity

--- app/data/models/generation.js
+++ app/data/models/generation.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   return sequelize.define('generation', {
     generationId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     statementData: DataTypes.JSON,
-    generated: DataTypes.DATE,
+    dateGenerated: DataTypes.DATE,
     filename: DataTypes.STRING
   },
   {
