34bf863c0e63c3b07bc541e84f5b6bcabfa3d45b
John Watson <johnwatson484@hotmail.com>
2022-08-04 22:12:33 +0100

    Swap margin

--- app/generator/styles.js
+++ app/generator/styles.js
@@ -2,21 +2,21 @@ module.exports = {
   header1: {
     fontSize: 24,
     bold: true,
-    lineHeight: 1.5
+    margin: [0, 0, 0, 10]
   },
   header2: {
     fontSize: 18,
     bold: true,
-    lineHeight: 1.5
+    margin: [0, 0, 0, 10]
   },
   header3: {
     fontSize: 12,
     bold: true,
-    lineHeight: 1.5
+    margin: [0, 0, 0, 10]
   },
   subTitle: {
     fontSize: 16,
-    lineHeight: 1.5
+    margin: [0, 0, 0, 10]
   },
   link: {
     decoration: 'underline',
