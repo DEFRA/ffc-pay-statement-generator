de12637893864f1f1654e9970efaea7d70bfa1b9
John Watson <johnwatson484@hotmail.com>
2022-08-05 18:29:41 +0100

    Update tests

--- test/unit/generator/create-filename.test.js
+++ test/unit/generator/create-filename.test.js
@@ -18,27 +18,21 @@ describe('create filename', () => {
     expect(result.endsWith(EXTENSION)).toBeTruthy()
   })
 
-  test('includes scheme for SFI', async () => {
+  test('includes scheme short name', async () => {
     mockStatement.scheme.shortName = 'SFI'
     const result = getFilename(mockStatement)
     expect(result).toContain('_SFI_')
   })
 
-  test('includes scheme for SFI Pilot', async () => {
-    mockStatement.scheme.shortName = 'SFIP'
+  test('includes FRN', async () => {
+    mockStatement.frn = 1234567890
     const result = getFilename(mockStatement)
-    expect(result).toContain('_SFIP_')
+    expect(result).toContain('_1234567890_')
   })
 
-  test('includes scheme for Lump Sums', async () => {
-    mockStatement.scheme.shortName = 'LSES'
+  test('removes spaces', async () => {
+    mockStatement.scheme.shortName = 'My Scheme'
     const result = getFilename(mockStatement)
-    expect(result).toContain('_LSES_')
-  })
-
-  test('includes scheme for Vets Visits', async () => {
-    mockStatement.scheme.shortName = 'VV'
-    const result = getFilename(mockStatement)
-    expect(result).toContain('_VV_')
+    expect(result).toContain('_MyScheme_')
   })
 })
