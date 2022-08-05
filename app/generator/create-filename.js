7e0ad6d17c5922a8dc9911b25769a8cea6bb422e
John Watson <johnwatson484@hotmail.com>
2022-08-05 15:50:20 +0100

    Update flow

--- app/generator/create-filename.js
+++ app/generator/create-filename.js
@@ -1,7 +1,7 @@
 const moment = require('moment')
 
 const createFilename = (statement) => {
-  return `FFC_PaymentStatement_${statement.scheme.shortName}_${statement.frn}_${moment().format('YYYYMMDDHHmmss')}.pdf`
+  return `FFC_PaymentStatement_${statement.scheme.shortName}_${statement.scheme.year}_${statement.frn}_${moment().format('YYYYMMDDHHmmss')}.pdf`
 }
 
 module.exports = createFilename
--- app/generator/index.js
+++ app/generator/index.js
@@ -16,18 +16,30 @@ const generateStatement = async (statement) => {
     defaultStyle: styles.default
   }
 
-  const filename = createFilename(statement)
-  const blobClient = await getOutboundBlobClient(filename)
   const pdfDoc = printer.createPdfKitDocument(docDefinition)
-  const chunks = []
-  pdfDoc.on('data', chunk => chunks.push(chunk))
-  pdfDoc.on('end', async () => {
-    const result = Buffer.concat(chunks)
-    await blobClient.upload(result, result.length)
-    await createLog(statement, filename)
-    console.log(`Generated statement: ${filename}`)
-  })
+  const filename = await publishPDF(pdfDoc, statement)
+  await createLog(statement, filename)
+}
+
+const publishPDF = (pdfDoc, statement) => {
   pdfDoc.end()
+  const filename = createFilename(statement)
+  return new Promise((resolve, reject) => {
+    const chunks = []
+    pdfDoc.on('data', chunk => chunks.push(chunk))
+    pdfDoc.on('end', async () => {
+      await uploadToStorage(chunks, filename)
+      resolve(filename)
+    })
+    pdfDoc.on('error', (err) => reject(err))
+  })
+}
+
+const uploadToStorage = async (chunks, filename) => {
+  const result = Buffer.concat(chunks)
+  const blobClient = await getOutboundBlobClient(filename)
+  await blobClient.upload(result, result.length)
+  console.log(`Generated statement: ${filename}`)
 }
 
 module.exports = generateStatement
