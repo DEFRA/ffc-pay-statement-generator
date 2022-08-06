83afebb12a4b8ebe0f3dab9e27ab24eb0bae1487
John Watson <johnwatson484@hotmail.com>
2022-08-06 14:05:14 +0100

    Add tests

--- test/unit/generator/content/part2.test.js
+++ test/unit/generator/content/part2.test.js
@@ -6,4 +6,30 @@ describe('generate part 2', () => {
     const result = part2(mockStatement)
     expect(result[0].text).toBe('Part 2. Calculation')
   })
+
+  test('includes introduction with scheme short name', () => {
+    const result = part2(mockStatement)
+    expect(result[1].text).toBe(`We calculated the total ${mockStatement.scheme.shortName} payment amount by adding together payments for all the standards which are part of your agreement, as shown in this table.`)
+  })
+
+  test('includes all help text lines', () => {
+    const result = part2(mockStatement)
+    expect(result[3].text.length).toBe(3)
+  })
+
+  test('includes help text introduction', () => {
+    const result = part2(mockStatement)
+    expect(result[3].text[0]).toBe('You can check which land parcels are part of each standard in the Rural Payments service at ')
+  })
+
+  test('includes help text link', () => {
+    const result = part2(mockStatement)
+    expect(result[3].text[1].link).toBe('https://www.gov.uk/claim-rural-payments')
+    expect(result[3].text[1].style).toBe('link')
+  })
+
+  test('includes help text full stopd', () => {
+    const result = part2(mockStatement)
+    expect(result[3].text[2]).toBe('.')
+  })
 })
