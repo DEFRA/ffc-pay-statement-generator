702ae1868065c5d7cadf883bb93cb1b1a37fe4ba
John Watson <johnwatson484@hotmail.com>
2022-08-06 20:54:26 +0100

    Add tests

--- test/unit/generator/content/part2.test.js
+++ test/unit/generator/content/part2.test.js
@@ -12,6 +12,21 @@ describe('generate part 2', () => {
     expect(result[1].text).toBe(`We calculated the total ${mockStatement.scheme.shortName} payment amount by adding together payments for all the standards which are part of your agreement, as shown in this table.`)
   })
 
+  test('includes all table columns', () => {
+    const result = part2(mockStatement)
+    expect(result[2].table.body.length).toBe(6)
+  })
+
+  test('includes all table columns', () => {
+    const result = part2(mockStatement)
+    expect(result[2].table.body[0].length).toBe(8)
+  })
+
+  test('includes all table rows', () => {
+    const result = part2(mockStatement)
+    expect(result[2].table.body.length).toBe(6)
+  })
+
   test('includes all help text lines', () => {
     const result = part2(mockStatement)
     expect(result[3].text.length).toBe(3)
@@ -28,7 +43,7 @@ describe('generate part 2', () => {
     expect(result[3].text[1].style).toBe('link')
   })
 
-  test('includes help text full stopd', () => {
+  test('includes help text full stop', () => {
     const result = part2(mockStatement)
     expect(result[3].text[2]).toBe('.')
   })
