5b8cfc479870328324e5ff6081b2f5910e538110
John Watson <johnwatson484@hotmail.com>
2022-08-07 16:04:52 +0100

    Add tests

--- test/unit/messaging/schemas/address.test.js
+++ test/unit/messaging/schemas/address.test.js
@@ -16,8 +16,68 @@ describe('address schema', () => {
     expect(result.error).toBeDefined()
   })
 
+  test('validates fail if empty postcode', () => {
+    mockAddress.postcode = ''
+    const result = schema.validate(mockAddress)
+    expect(result.error).toBeDefined()
+  })
+
   test('validates success if missing line 1', () => {
     delete mockAddress.line1
+    const result = schema.validate(mockAddress)
+    expect(result.error).toBeUndefined()
+  })
+
+  test('validates success if empty line 1', () => {
+    mockAddress.line1 = ''
+    const result = schema.validate(mockAddress)
+    expect(result.error).toBeUndefined()
+  })
+
+  test('validates success if missing line 2', () => {
+    delete mockAddress.line2
+    const result = schema.validate(mockAddress)
+    expect(result.error).toBeUndefined()
+  })
+
+  test('validates success if empty line 2', () => {
+    mockAddress.line2 = ''
+    const result = schema.validate(mockAddress)
+    expect(result.error).toBeUndefined()
+  })
+
+  test('validates success if missing line 3', () => {
+    delete mockAddress.line3
+    const result = schema.validate(mockAddress)
+    expect(result.error).toBeUndefined()
+  })
+
+  test('validates success if empty line 3', () => {
+    mockAddress.line3 = ''
+    const result = schema.validate(mockAddress)
+    expect(result.error).toBeUndefined()
+  })
+
+  test('validates success if missing line 4', () => {
+    delete mockAddress.line4
+    const result = schema.validate(mockAddress)
+    expect(result.error).toBeUndefined()
+  })
+
+  test('validates success if empty line 4', () => {
+    mockAddress.line4 = ''
+    const result = schema.validate(mockAddress)
+    expect(result.error).toBeUndefined()
+  })
+
+  test('validates success if missing line 5', () => {
+    delete mockAddress.line5
+    const result = schema.validate(mockAddress)
+    expect(result.error).toBeUndefined()
+  })
+
+  test('validates success if empty line 5', () => {
+    mockAddress.line5 = ''
     const result = schema.validate(mockAddress)
     expect(result.error).toBeUndefined()
   })
