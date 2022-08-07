36683b0a24fab0520cccaca447eb7d48f13ff187
John Watson <johnwatson484@hotmail.com>
2022-08-07 14:48:22 +0100

    Swap validation

--- app/messaging/process-statement-message.js
+++ app/messaging/process-statement-message.js
@@ -1,19 +1,20 @@
 const generateStatement = require('../generator')
 const util = require('util')
-const schema = require('./schemas/statement')
+const validateStatement = require('./validate-statement')
+const { VALIDATION } = require('../errors')
 
 const processStatementMessage = async (message, receiver) => {
   try {
     const statement = message.body
     console.log('Statement received:', util.inspect(statement, false, null, true))
-    const validationResult = schema.validate(statement, { abortEarly: false })
-    if (validationResult.error) {
-      throw new Error(`Statement content is invalid, ${validationResult.error.message}`)
-    }
+    validateStatement(statement)
     await generateStatement(statement)
     await receiver.completeMessage(message)
   } catch (err) {
     console.error('Unable to process statement:', err)
+    if(err.category === VALIDATION) {
+      await receiver.deadLetterMessage(message)
+    }
   }
 }
 
