1be5c047fe13245d5fce8a1b1ed456d22e942a09
John Watson <johnwatson484@hotmail.com>
2022-08-08 09:51:44 +0100

    Add content

--- app/generator/content/part3.js
+++ app/generator/content/part3.js
@@ -1,7 +1,7 @@
 const part3 = (_statement) => {
   return [
     { text: 'Part 3. More support', style: 'header2' },
-    'If you think your payment is wrong you should:',
+    'If you think your payment is wrong you should:\n\n',
     {
       ul: [
         {
@@ -21,7 +21,7 @@ const part3 = (_statement) => {
       text: [
         'If you still do not agree with the amount you have been paid, contact us within 3 months of the payment date at ',
         { text: 'ruralpayments@defra.gov.uk', link: 'ruralpayments@defra.gov.uk', style: 'link' },
-        '.'
+        '.\n\n'
       ]
     },
     {
