569604a0e74d185c70d5b460b4c25e6fa119036f
John Watson <johnwatson484@hotmail.com>
2022-08-12 16:57:39 +0100

    Update definition

--- app/generator/index.js
+++ app/generator/index.js
@@ -3,16 +3,11 @@ const moment = require('moment')
 const fonts = require('./fonts')
 const saveLog = require('./save-log')
 const publish = require('./publish')
+const getDefinition = require('./get-definition')
 const printer = new PdfPrinter(fonts)
 
 const generateStatement = async (statement) => {
-  const docDefinition = {
-    pageSize: 'A4',
-    content: generateContent(statement),
-    styles,
-    defaultStyle: styles.default
-  }
-
+  const docDefinition = getDefinition(statement)
   const timestamp = new Date()
   const pdfDoc = printer.createPdfKitDocument(docDefinition)
   const filename = await publish(pdfDoc, statement, moment(timestamp).format('YYYYMMDDHHmmssSS'))
