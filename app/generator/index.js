fd90607ba0b61f27079712928355b8ba0638e607
John Watson <johnwatson484@hotmail.com>
2022-08-12 16:34:06 +0100

    Split doc definition into it's own module

--- app/generator/index.js
+++ app/generator/index.js
@@ -1,20 +1,13 @@
 const PdfPrinter = require('pdfmake')
 const moment = require('moment')
 const fonts = require('./fonts')
-const styles = require('./styles')
-const generateContent = require('./content')
 const saveLog = require('./save-log')
 const publish = require('./publish')
+const getDefinition = require('./get-definition')
 const printer = new PdfPrinter(fonts)
 
 const generateStatement = async (statement) => {
-  const docDefinition = {
-    pageSize: 'A4',
-    content: generateContent(statement),
-    styles,
-    defaultStyle: styles.default
-  }
-
+  const docDefinition = getDefinition(statement)
   const timestamp = new Date()
   const pdfDoc = printer.createPdfKitDocument(docDefinition)
   const filename = await publish(pdfDoc, statement, moment(timestamp).format('YYYYMMDDHHmmssSS'))
---
+++ app/generator/page-sizes.js
@@ -0,0 +1,3 @@
+module.exports = {
+  A4: 'A4'
+}
