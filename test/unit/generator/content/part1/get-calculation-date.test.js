9b4e1db11b03611b4886b7184ec3fa8f1c3474ea
John Watson <johnwatson484@hotmail.com>
2022-08-16 12:14:28 +0100

    Add tests

---
+++ test/unit/generator/content/part1/get-calculation-date.test.js
@@ -0,0 +1,39 @@
+const getCalculationDate = require('../../../../../app/generator/content/part1/get-calculation-date')
+const calculated = '1 July 2022'
+
+describe('get calculation date', () => {
+  test('returns two columns', () => {
+    const result = getCalculationDate(calculated)
+    expect(result.columns.length).toBe(2)
+  })
+
+  test('returns title column text', () => {
+    const result = getCalculationDate(calculated)
+    expect(result.columns[0].text).toBe('Calculation date:')
+  })
+
+  test('returns title column width', () => {
+    const result = getCalculationDate(calculated)
+    expect(result.columns[0].width).toBe(200)
+  })
+
+  test('returns business name column text', () => {
+    const result = getCalculationDate(calculated)
+    expect(result.columns[1].text).toBe('1 July 2022')
+  })
+
+  test('returns business name column width', () => {
+    const result = getCalculationDate(calculated)
+    expect(result.columns[1].width).toBe('*')
+  })
+
+  test('returns column style', () => {
+    const result = getCalculationDate(calculated)
+    expect(result.style).toBe('column')
+  })
+
+  test('returns column gap', () => {
+    const result = getCalculationDate(calculated)
+    expect(result.columnGap).toBe(10)
+  })
+})
---
+++ test/unit/generator/content/part1/get-table.test.js
@@ -0,0 +1,61 @@
+const getTable = require('../../../../../app/generator/content/part1/get-table')
+let mockStatement
+let latestPeriod
+
+describe('get table', () => {
+  beforeEach(() => {
+    mockStatement = JSON.parse(JSON.stringify(require('../../../../mocks/statement-data')))
+    latestPeriod = 'July to October 2022'
+  })
+
+  test('includes table with a single row', () => {
+    const result = getTable(mockStatement.scheme, mockStatement.payments[0], latestPeriod)
+    expect(result.table.body.length).toBe(1)
+  })
+
+  test('includes table with a single column', () => {
+    const result = getTable(mockStatement.scheme, mockStatement.payments[0], latestPeriod)
+    expect(result.table.body[0].length).toBe(1)
+  })
+
+  test('includes table cell with background colour', () => {
+    const result = getTable(mockStatement.scheme, mockStatement.payments[0], latestPeriod)
+    expect(result.table.body[0][0].fillColor).toBe('#d9d9d9')
+  })
+
+  test('includes payment schedule', () => {
+    const result = getTable(mockStatement.scheme, mockStatement.payments[0], latestPeriod)
+    expect(result.table.body[0][0].stack[0].text).toBe('Your quarterly SFI payment for July to October 2022 is £242.15')
+  })
+
+  test('includes payment schedule when latest period single month', () => {
+    latestPeriod = 'July 2022'
+    const result = getTable(mockStatement.scheme, mockStatement.payments[0], latestPeriod)
+    expect(result.table.body[0][0].stack[0].text).toBe('Your quarterly SFI payment for July 2022 is £242.15')
+  })
+
+  test('includes payment date', () => {
+    const result = getTable(mockStatement.scheme, mockStatement.payments[0], latestPeriod)
+    expect(result.table.body[0][0].stack[1]).toMatch('\nWe will pay this into your account on 1 July 2022.\n')
+  })
+
+  test('includes calculation date title', () => {
+    const result = getTable(mockStatement.scheme, mockStatement.payments[0], latestPeriod)
+    expect(result.table.body[0][0].stack[2].columns[0].text).toBe('Calculation date:')
+  })
+
+  test('includes calculation date value', () => {
+    const result = getTable(mockStatement.scheme, mockStatement.payments[0], latestPeriod)
+    expect(result.table.body[0][0].stack[2].columns[1].text).toBe('16 June 2022')
+  })
+
+  test('includes reference title', () => {
+    const result = getTable(mockStatement.scheme, mockStatement.payments[0], latestPeriod)
+    expect(result.table.body[0][0].stack[3].columns[0].text).toBe('Payment reference number:')
+  })
+
+  test('includes reference value', () => {
+    const result = getTable(mockStatement.scheme, mockStatement.payments[0], latestPeriod)
+    expect(result.table.body[0][0].stack[3].columns[1].text).toBe('PY1234567')
+  })
+})
