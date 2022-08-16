23ec454d73ea7fdcb6d95795979f4ed8c5bee1eb
John Watson <johnwatson484@hotmail.com>
2022-08-16 18:17:41 +0100

    Add tests

--- app/generator/content/part1/get-latest-payment.js
+++ app/generator/content/part1/get-latest-payment.js
@@ -2,8 +2,18 @@ const moment = require('moment')
 const SETTLEMENT_DATE_FORMAT = 'DD MMMM YYYY'
 
 const getLatestPayment = (payments) => {
+  const validPayments = payments.filter(x => moment(x.settled, SETTLEMENT_DATE_FORMAT).isValid())
+  if (validPayments.length === 0) {
+    return payments[payments.length - 1]
+  }
+  return getLatestFromValid(validPayments)
+}
+
+const getLatestFromValid = (payments) => {
   return payments.reduce((x, y) => {
-    return moment(x.settled, SETTLEMENT_DATE_FORMAT) > moment(y.settled, SETTLEMENT_DATE_FORMAT) ? x : y
+    const currentDate = moment(x.settled, SETTLEMENT_DATE_FORMAT)
+    const previousDate = moment(y.settled, SETTLEMENT_DATE_FORMAT)
+    return currentDate && currentDate.isAfter(previousDate) ? x : y
   })
 }
 
