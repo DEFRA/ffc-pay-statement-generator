35ec44f50931d5229247b0c75810a18648ba3657
John Watson <42574977+johnwatson484@users.noreply.github.com>
2022-08-18 09:10:11 +0100

    Add part 3 (#6)

--- app/generator/content/part3.js
+++ app/generator/content/part3.js
@@ -1,7 +1,42 @@
-const part3 = (_statement) => {
-  return [
-    { text: 'Part 3. More support', style: 'header2' }
-  ]
+const part3 = () => {
+  return {
+    stack: [
+      { text: 'Part 3. More support', style: 'header2' },
+      'If you think your payment is wrong you should:\n\n',
+      {
+        ul: [
+          {
+            text: [
+              'check your land parcel information in the Rural Payments service at ',
+              { text: 'www.gov.uk/claim-rural-payments', link: 'https://www.gov.uk/claim-rural-payments', style: 'link' }
+            ]
+          }, {
+            text: [
+              'review the SFI scheme information at ',
+              { text: 'www.gov.uk/sfi', link: 'https://www.gov.uk/sfi', style: 'link' },
+              '\n\n'
+            ]
+          }
+        ]
+      },
+      {
+        text: [
+          'If you still do not agree with the amount you have been paid, contact us within 3 months of the payment date at ',
+          { text: 'ruralpayments@defra.gov.uk', link: 'mailto:ruralpayments@defra.gov.uk', style: 'link' },
+          '.\n\n'
+        ]
+      },
+      {
+        text: [
+          'Find other ways to get in touch at ',
+          { text: 'www.gov.uk/guidance/contacting-the-rpa-about-sfi', link: 'https://www.gov.uk/guidance/contacting-the-rpa-about-sfi', style: 'link' },
+          '.'
+        ]
+      },
+      { text: 'To receive this statement in large print or alternative format, contact the Rural Payments Agency', style: 'header2' }
+    ],
+    unbreakable: true
+  }
 }
 
 module.exports = part3
