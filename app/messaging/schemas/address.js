ad0a577388329075b1643af6ffae5e8a11b8d0e6
John Watson <42574977+johnwatson484@users.noreply.github.com>
2022-10-04 15:37:59 +0100

    Fix validation (#16)

--- app/messaging/schemas/address.js
+++ app/messaging/schemas/address.js
@@ -1,10 +1,10 @@
 const Joi = require('joi')
 
 module.exports = Joi.object({
-  line1: Joi.string().optional().allow(''),
-  line2: Joi.string().optional().allow(''),
-  line3: Joi.string().optional().allow(''),
-  line4: Joi.string().optional().allow(''),
-  line5: Joi.string().optional().allow(''),
+  line1: Joi.string().optional().allow('', null),
+  line2: Joi.string().optional().allow('', null),
+  line3: Joi.string().optional().allow('', null),
+  line4: Joi.string().optional().allow('', null),
+  line5: Joi.string().optional().allow('', null),
   postcode: Joi.string().required()
 }).required()
