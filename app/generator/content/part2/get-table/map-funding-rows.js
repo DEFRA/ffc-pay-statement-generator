aba953fab09fd4f152c93442bc398295569ac8d6
John Watson <johnwatson484@hotmail.com>
2022-10-27 17:43:49 +0100

    Format zero value row

--- app/generator/content/part2/get-table/map-funding-rows.js
+++ app/generator/content/part2/get-table/map-funding-rows.js
@@ -6,13 +6,17 @@ const mapFundingRows = (fundings) => {
   return orderedFundings.map(x => ([
     x.name,
     x.level,
-    { text: x.rate, style: 'tableNumber' },
-    { text: x.area, style: 'tableNumber' },
+    { text: formatZeroValues(x.rate), style: 'tableNumber' },
+    { text: formatZeroValues(x.area), style: 'tableNumber' },
     { text: `${toCurrencyString(x.annualValue)}`, style: 'tableNumber' },
     { text: `${toCurrencyString(x.quarterlyValue)}`, style: 'tableNumber' },
     { text: `${toCurrencyString(x.quarterlyReduction)}`, style: 'tableNumber' },
     { text: `£${x.quarterlyPayment}`, style: 'tableNumber' }
   ]))
+}
+
+const formatZeroValues = (value) => {
+  return Number(value) === 0 ? '' : value
 }
 
 module.exports = mapFundingRows
