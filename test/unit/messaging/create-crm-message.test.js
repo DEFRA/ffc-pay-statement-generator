6cc0ddc3409b1f27b48c6e79af3b7ae457c3493c
John Watson <johnwatson484@hotmail.com>
2023-01-24 16:19:37 +0000

    Update tests

--- test/unit/messaging/create-crm-message.test.js
+++ test/unit/messaging/create-crm-message.test.js
@@ -17,13 +17,13 @@ const schema = require('../../../app/messaging/crm/crm-schema')
 const { statementReceiverApiVersion, statementReceiverEndpoint } = require('../../../app/config')
 const createCrmMessage = require('../../../app/messaging/crm/create-crm-message')
 const mockStatement = require('../../mocks/mock-statement')
-const { STATEMENT } = require('../../../app/document-types')
+const { STATEMENT, SCHEDULE } = require('../../../app/document-types')
 const FILENAME = 'FFC_PaymentStatement_SFI_2022_1234567890_2022080515301012.pdf'
 
 let crmValid
 let crmMessage
 
-describe('send crm message', () => {
+describe('send crm message for statement', () => {
   beforeEach(() => {
     jest.clearAllMocks()
 
@@ -66,6 +66,55 @@ describe('send crm message', () => {
 
     const wrapper = async () => {
       createCrmMessage(mockStatement, FILENAME, STATEMENT)
+    }
+
+    expect(wrapper).rejects.toThrow(Error)
+  })
+})
+
+describe('send crm message for payment schedule', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    crmValid = {
+      sbi: mockStatement.sbi,
+      frn: mockStatement.frn,
+      apiLink: `${statementReceiverEndpoint}/${statementReceiverApiVersion}/statements/statement/${FILENAME}`,
+      scheme: mockStatement.scheme.shortName,
+      documentType: SCHEDULE.name
+    }
+
+    crmMessage = {
+      body: {
+        ...crmValid
+      },
+      type: 'uk.gov.pay.schedule.crm',
+      source: 'ffc-pay-statement-generator'
+    }
+
+    schema.validate.mockReturnValue({ value: crmValid })
+  })
+
+  test('should call schema.validate when statement and filename are given', async () => {
+    createCrmMessage(mockStatement, FILENAME, SCHEDULE)
+    expect(schema.validate).toHaveBeenCalled()
+  })
+
+  test('should call schema.validate once when statement and filename are given', async () => {
+    createCrmMessage(mockStatement, FILENAME, SCHEDULE)
+    expect(schema.validate).toHaveBeenCalledTimes(1)
+  })
+
+  test('should return valid message when statement and filename are given', async () => {
+    const result = createCrmMessage(mockStatement, FILENAME, SCHEDULE)
+    expect(result).toStrictEqual(crmMessage)
+  })
+
+  test('should throw Error when schema validate throws Error', async () => {
+    schema.validate.mockReturnValue({ error: 'Not a valid object' })
+
+    const wrapper = async () => {
+      createCrmMessage(mockStatement, FILENAME, SCHEDULE)
     }
 
     expect(wrapper).rejects.toThrow(Error)
