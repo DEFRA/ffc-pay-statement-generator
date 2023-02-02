29e209e5d649161735816a6fde8f6e440c693532
John Watson <johnwatson484@hotmail.com>
2023-02-02 18:14:36 +0000

    Simplify test

--- test/unit/generator/content/schedule/part1/get-adjustment.test.js
+++ test/unit/generator/content/schedule/part1/get-adjustment.test.js
@@ -1,39 +1,34 @@
 const { getAdjustment } = require('../../../../../../app/generator/content/schedule/part1/get-adjustment')
 
+let adjustment
+
 describe('get adjustment', () => {
-  test('should return current agreement value', () => {
-    const result = getAdjustment({
+  beforeEach(() => {
+    adjustment = {
       currentValue: 100,
       newValue: 200,
       adjustmentValue: 100
-    })
+    }
+  })
+
+  test('should return current agreement value', () => {
+    const result = getAdjustment(adjustment)
     expect(result[0]).toBe('Current agreement value: 100\n')
   })
 
   test('should return new agreement value', () => {
-    const result = getAdjustment({
-      currentValue: 100,
-      newValue: 200,
-      adjustmentValue: 100
-    })
+    const result = getAdjustment(adjustment)
     expect(result[1]).toBe('New agreement value: 200\n')
   })
 
   test('should return top up amount', () => {
-    const result = getAdjustment({
-      currentValue: 100,
-      newValue: 200,
-      adjustmentValue: 100
-    })
+    const result = getAdjustment(adjustment)
     expect(result[2]).toBe('Top up amount: 100')
   })
 
   test('should return reduction amount', () => {
-    const result = getAdjustment({
-      currentValue: 200,
-      newValue: 100,
-      adjustmentValue: -100
-    })
+    adjustment.adjustmentValue = -100
+    const result = getAdjustment(adjustment)
     expect(result[2]).toBe('Reduction amount: -100')
   })
 })
