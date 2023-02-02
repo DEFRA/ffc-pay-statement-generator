f7c2f630de9c71b6f11e7b520e343e18fe045dee
John Watson <johnwatson484@hotmail.com>
2023-02-02 18:11:41 +0000

    Update joi messages

--- app/messaging/schemas/frn.js
+++ app/messaging/schemas/frn.js
@@ -1,3 +1,9 @@
 const Joi = require('joi')
 
-module.exports = Joi.number().integer().min(1000000000).max(9999999999).required()
+module.exports = Joi.number().integer().min(1000000000).max(9999999999).required().messages({
+  'number.base': 'FRN must be a number',
+  'number.integer': 'FRN must be an integer',
+  'number.min': 'FRN must be at least 10 digits',
+  'number.max': 'FRN must be no more than 10 digits',
+  'any.required': 'is required'
+})
--- app/messaging/schemas/sbi.js
+++ app/messaging/schemas/sbi.js
@@ -1,3 +1,9 @@
 const Joi = require('joi')
 
-module.exports = Joi.number().integer().min(105000000).max(999999999).required()
+module.exports = Joi.number().integer().min(105000000).max(999999999).required().messages({
+  'number.base': 'SBI must be a number',
+  'number.integer': 'SBI must be an integer',
+  'number.min': 'SBI must be at least 9 digits',
+  'number.max': 'SBI must be no more than 9 digits',
+  'any.required': 'is required'
+})
