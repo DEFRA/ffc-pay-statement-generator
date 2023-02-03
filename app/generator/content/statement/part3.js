bb633681a2f24c86a9aed52162b22fca721e5287
kirsten.williamson <kirsten.williamson@kainos.com>
2023-02-03 15:14:26 +0000

    Removed unnecessary line from part3.js

--- app/generator/content/statement/part3.js
+++ app/generator/content/statement/part3.js
@@ -23,7 +23,7 @@ const part3 = () => {
         text: [
           'If you still have questions, you can ',
           { text: 'use the query form', link: 'https://www.gov.uk/government/publications/contact-the-rpa-about-sfi', style: 'link' },
-          ' to contact us. You should contact us within 3 months of receiving your payment.\n\n'
+          ' to contact us.\n\n'
         ]
       },
       {
