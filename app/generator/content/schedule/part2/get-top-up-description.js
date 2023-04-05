42be84bad6540595fed4ef55399393626aa786b3
Marc Templeton <marcte@kainos.com>
2023-04-05 15:19:11 +0100

    added topup descr

--- app/generator/content/schedule/part2/get-top-up-description.js
+++ app/generator/content/schedule/part2/get-top-up-description.js
@@ -6,7 +6,7 @@ const getTopUpDescription = (schedule) => {
       `\n\nWe recently sent you a letter to tell you the annual value of your ${schedule.scheme.name} (${schedule.scheme.shortName}) agreement has increased.`,
       '\nThe payment schedule sets out the payments you\'ll receive for the rest of the current scheme year.\n',
       '\nThe new annual value of your agreement will be split equally between 4 quarterly payments for the year. You may receive either or both of the following:\n\n',
-      { ul: ['an immediate payment - this is a one-off amount which will be a top-up to any quarterly payments you\'ve already received for this scheme yea', 'an increase to quarterly payments you\'ve not yet received'], listStyle: 'square' },
+      { ul: ['an immediate payment - this is a one-off amount which will be a top-up to any quarterly payments you\'ve already received for this scheme year', 'an increase to quarterly payments you\'ve not yet received'], listStyle: 'square' },
       { text: 'Payment schedule', style: 'header2' },
       'The table below explains when, and how much you\'ll be paid.\n',
       getTopUpSummary(schedule.adjustment)
---
+++ test/unit/generator/content/schedule/part2/get-top-up-description.test.js
@@ -0,0 +1,70 @@
+const getTopUpDescription = require('../../../../../../app/generator/content/schedule/part2/get-top-up-description')
+
+const { topUpSchedule } = require('../../../../../mocks/mock-schedule')
+
+describe('Get top-up description', () => {
+  test('should return an object', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(result).toBeInstanceOf(Object)
+  })
+
+  test('should return an object with 2 keys', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(Object.keys(result)).toHaveLength(2)
+  })
+
+  test('should return an object with keys ["stack", "unbreakable"]', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(Object.keys(result)).toStrictEqual(['stack', 'unbreakable'])
+  })
+
+  test('should return an true for key unbreakable', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(result.unbreakable).toBe(true)
+  })
+
+  test('should return an array for key stack', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(Array.isArray(result.stack)).toBe(true)
+  })
+
+  test('should return an array of length 7 for key stack', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(result.stack).toHaveLength(7)
+  })
+
+  test('should return "\n\nWe recently sent you a letter to tell you the annual value of your topUpSchedule.scheme.name} (topUpSchedule.scheme.shortName) agreement has increased." for key stack 1 entry', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(result.stack[0]).toBe(`\n\nWe recently sent you a letter to tell you the annual value of your ${topUpSchedule.scheme.name} (${topUpSchedule.scheme.shortName}) agreement has increased.`)
+  })
+
+  test('should return "\nThe payment schedule sets out the payments youll receive for the rest of the current scheme year.\n" for key stack 2 entry', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(result.stack[1]).toBe('\nThe payment schedule sets out the payments you\'ll receive for the rest of the current scheme year.\n')
+  })
+
+  test('should return "\nThe new annual value of your agreement will be split equally between 4 quarterly payments for the year. You may receive either or both of the following:\n\n" for key stack 3 entry', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(result.stack[2]).toBe('\nThe new annual value of your agreement will be split equally between 4 quarterly payments for the year. You may receive either or both of the following:\n\n')
+  })
+
+  test('should return "{ ul: ["an immediate payment - this is a one-off amount which will be a top-up to any quarterly payments youve already received for this scheme year", "an increase to quarterly payments youve not yet received"], listStyle: "square" }" for key stack 4 entry', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(result.stack[3]).toStrictEqual({ ul: ['an immediate payment - this is a one-off amount which will be a top-up to any quarterly payments you\'ve already received for this scheme year', 'an increase to quarterly payments you\'ve not yet received'], listStyle: 'square' })
+  })
+
+  test('should return "{ text: "Payment schedule", style: "header2" }" for key stack 5 entry', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(result.stack[4]).toStrictEqual({ text: 'Payment schedule', style: 'header2' })
+  })
+
+  test('should return "The table below explains when, and how much youll be paid.\n" for key stack 6 entry', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(result.stack[5]).toBe('The table below explains when, and how much you\'ll be paid.\n')
+  })
+
+  test('should return object for key stack 7 entry', () => {
+    const result = getTopUpDescription(topUpSchedule)
+    expect(result.stack[6]).toBeInstanceOf(Object)
+  })
+})
