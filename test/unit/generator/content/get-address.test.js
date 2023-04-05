ef152e3b11016b3dae3adf931de91733420a4344
Marc Templeton <marcte@kainos.com>
2023-04-05 12:21:14 +0100

    updating address

--- test/unit/generator/content/get-address.test.js
+++ test/unit/generator/content/get-address.test.js
@@ -1,183 +1,152 @@
 const { getAddress } = require('../../../../app/generator/content/get-address')
-const businessName = 'Mr A Farmer'
+const businessName = require('../../../mocks/components/business-name')
+const fullAddress = require('../../../mocks/components/address')
+
+let address
 
 describe('get address', () => {
-  test('returns one line address if on business name', () => {
-    const address = {}
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\n')
+  describe('when address is empty', () => {
+    beforeEach(() => {
+      address = {}
+    })
+
+    test('should return business name and new line', () => {
+      const result = getAddress(businessName, address)
+      expect(result.text).toMatch(`${businessName}\n`)
+    })
+
+    test('should return style as address', () => {
+      const result = getAddress(businessName, address)
+      expect(result.style).toMatch('address')
+    })
+
+    test('should return correct x-axis location', () => {
+      const result = getAddress(businessName, address)
+      expect(result.absolutePosition.x).toBe(28.35)
+    })
+
+    test('should return correct y-axis location', () => {
+      const result = getAddress(businessName, address)
+      expect(result.absolutePosition.y).toBe(99.225)
+    })
   })
 
-  test('returns two line address if only postcode', () => {
-    const address = {
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nNE1 1AA')
+  describe('when full address is given', () => {
+    beforeEach(() => {
+      address = fullAddress
+    })
+
+    test('should return business name and each address value in order separated by new lines', () => {
+      const result = getAddress(businessName, address)
+      expect(result.text).toMatch(`${businessName}\n${address.line1}\n${address.line2}\n${address.line3}\n${address.line4}\n${address.line5}\n${address.postcode}\n`)
+    })
+
+    test('should return style as address', () => {
+      const result = getAddress(businessName, address)
+      expect(result.style).toMatch('address')
+    })
+
+    test('should return correct x-axis location', () => {
+      const result = getAddress(businessName, address)
+      expect(result.absolutePosition.x).toBe(28.35)
+    })
+
+    test('should return correct y-axis location', () => {
+      const result = getAddress(businessName, address)
+      expect(result.absolutePosition.y).toBe(99.225)
+    })
   })
 
-  test('returns three line address if only line 1', () => {
-    const address = {
-      line1: 'A Farm',
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nNE1 1AA')
+  describe('when address values have undefined', () => {
+    beforeEach(() => {
+      address = {
+        ...fullAddress,
+        line1: undefined,
+        line4: undefined,
+        postcode: undefined
+      }
+    })
+
+    test('should return business name and each address value in order separated by new lines ignoring those which are undefined', () => {
+      const result = getAddress(businessName, address)
+      expect(result.text).toMatch(`${businessName}\n${address.line2}\n${address.line3}\n${address.line5}\n`)
+    })
+
+    test('should return style as address', () => {
+      const result = getAddress(businessName, address)
+      expect(result.style).toMatch('address')
+    })
+
+    test('should return correct x-axis location', () => {
+      const result = getAddress(businessName, address)
+      expect(result.absolutePosition.x).toBe(28.35)
+    })
+
+    test('should return correct y-axis location', () => {
+      const result = getAddress(businessName, address)
+      expect(result.absolutePosition.y).toBe(99.225)
+    })
   })
 
-  test('returns three line address if only line 2', () => {
-    const address = {
-      line2: 'A Farm',
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nNE1 1AA')
+  describe('when address values have null', () => {
+    beforeEach(() => {
+      address = {
+        ...fullAddress,
+        line1: null,
+        line4: null,
+        postcode: null
+      }
+    })
+
+    test('should return business name and each address value in order separated by new lines ignoring those which are null', () => {
+      const result = getAddress(businessName, address)
+      expect(result.text).toMatch(`${businessName}\n${address.line2}\n${address.line3}\n${address.line5}\n`)
+    })
+
+    test('should return style as address', () => {
+      const result = getAddress(businessName, address)
+      expect(result.style).toMatch('address')
+    })
+
+    test('should return correct x-axis location', () => {
+      const result = getAddress(businessName, address)
+      expect(result.absolutePosition.x).toBe(28.35)
+    })
+
+    test('should return correct y-axis location', () => {
+      const result = getAddress(businessName, address)
+      expect(result.absolutePosition.y).toBe(99.225)
+    })
   })
 
-  test('returns three line address if only line 3', () => {
-    const address = {
-      line3: 'A Farm',
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nNE1 1AA')
-  })
+  describe('when address values have empty', () => {
+    beforeEach(() => {
+      address = {
+        ...fullAddress,
+        line1: '',
+        line4: '',
+        postcode: ''
+      }
+    })
 
-  test('returns three line address if only line 4', () => {
-    const address = {
-      line4: 'A Farm',
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nNE1 1AA')
-  })
+    test('should return business name and each address value in order separated by new lines ignoring those which are empty', () => {
+      const result = getAddress(businessName, address)
+      expect(result.text).toMatch(`${businessName}\n${address.line2}\n${address.line3}\n${address.line5}\n`)
+    })
 
-  test('returns three line address if only line 5', () => {
-    const address = {
-      line5: 'A Farm',
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nNE1 1AA')
-  })
+    test('should return style as address', () => {
+      const result = getAddress(businessName, address)
+      expect(result.style).toMatch('address')
+    })
 
-  test('returns four line address if lines in order', () => {
-    const address = {
-      line1: 'A Farm',
-      line2: 'A Place',
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nA Place\nNE1 1AA')
-  })
+    test('should return correct x-axis location', () => {
+      const result = getAddress(businessName, address)
+      expect(result.absolutePosition.x).toBe(28.35)
+    })
 
-  test('returns four line address if lines in not order', () => {
-    const address = {
-      line1: 'A Farm',
-      line5: 'A Place',
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nA Place\nNE1 1AA')
-  })
-
-  test('returns full address', () => {
-    const address = {
-      line1: 'A Farm',
-      line2: 'A Place',
-      line3: 'A Location',
-      line4: 'A Town',
-      line5: 'A County',
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nA Place\nA Location\nA Town\nA County\nNE1 1AA')
-  })
-
-  test('ignores undefined lines', () => {
-    const address = {
-      line1: 'A Farm',
-      line2: undefined,
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nNE1 1AA')
-  })
-
-  test('ignores undefined lines if in middle lines', () => {
-    const address = {
-      line1: 'A Farm',
-      line2: undefined,
-      line3: 'A Place',
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nA Place\nNE1 1AA')
-  })
-
-  test('ignores null lines', () => {
-    const address = {
-      line1: 'A Farm',
-      line2: null,
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nNE1 1AA')
-  })
-
-  test('ignores null lines if in middle lines', () => {
-    const address = {
-      line1: 'A Farm',
-      line2: null,
-      line3: 'A Place',
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nA Place\nNE1 1AA')
-  })
-
-  test('ignores empty lines', () => {
-    const address = {
-      line1: 'A Farm',
-      line2: '',
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nNE1 1AA')
-  })
-
-  test('ignores empty lines if in middle lines', () => {
-    const address = {
-      line1: 'A Farm',
-      line2: '',
-      line3: 'A Place',
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.text).toMatch('Mr A Farmer\nA Farm\nA Place\nNE1 1AA')
-  })
-
-  test('returns address style', () => {
-    const address = {
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.style).toMatch('address')
-  })
-
-  test('returns correct location x axis', () => {
-    const address = {
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.absolutePosition.x).toBe(28.35)
-  })
-
-  test('returns correct location y axis', () => {
-    const address = {
-      postcode: 'NE1 1AA'
-    }
-    const result = getAddress(businessName, address)
-    expect(result.absolutePosition.y).toBe(99.225)
+    test('should return correct y-axis location', () => {
+      const result = getAddress(businessName, address)
+      expect(result.absolutePosition.y).toBe(99.225)
+    })
   })
 })
