c772fa85526705c0880ff1be2c1f6fc0bc266191
Marc Templeton <marcte@kainos.com>
2023-04-06 11:47:27 +0100

    adding req payment type

--- app/messaging/schemas/payment-schedule.js
+++ app/messaging/schemas/payment-schedule.js
@@ -1,9 +1,11 @@
 const Joi = require('joi')
 
+const { IMMEDIATE, QUARTERLY } = require('../../constants/payment-types')
+
 module.exports = Joi.object({
   order: Joi.number().required(),
   dueDate: Joi.string().optional().allow('', null),
   period: Joi.string().optional().allow('', null),
   value: Joi.string().required(),
-  paymentType: Joi.string().required()
+  paymentType: Joi.string().valid([IMMEDIATE, QUARTERLY]).required()
 })
