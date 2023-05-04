e920da9cf7e28c90ff8bfabff6c954bb3ef0fc9d
Marc Templeton <marcte@kainos.com>
2023-05-04 16:37:13 +0100

    updated data model

--- app/data/models/generation.js
+++ app/data/models/generation.js
@@ -3,7 +3,21 @@ module.exports = (sequelize, DataTypes) => {
     generationId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     statementData: DataTypes.JSON,
     dateGenerated: DataTypes.DATE,
-    filename: DataTypes.STRING
+    filename: DataTypes.STRING,
+    frn: DataTypes.BIGINT,
+    sbi: DataTypes.INTEGER,
+    businessName: DataTypes.STRING,
+    addressLine1: DataTypes.STRING,
+    addressLine2: DataTypes.STRING,
+    addressLine3: DataTypes.STRING,
+    addressLine4: DataTypes.STRING,
+    addressLine5: DataTypes.STRING,
+    postcode: DataTypes.STRING,
+    email: DataTypes.STRING,
+    schemeName: DataTypes.STRING,
+    schemeShortName: DataTypes.STRING,
+    schemeYear: DataTypes.STRING,
+    schemeFrequency: DataTypes.STRING
   },
   {
     tableName: 'generations',
