c63050fac6ed9c0ee16cd22541c99dc82c5598cf
Marc Templeton <marcte@kainos.com>
2023-05-12 14:25:47 +0100

    wip testing documentRef validation

--- test/unit/messaging/validate-request.test.js
+++ test/unit/messaging/validate-request.test.js
@@ -1,11 +1,80 @@
-const { validateRequest } = require('../../../app/messaging/validate-request')
 const { STATEMENT, SCHEDULE } = require('../../../app/constants/document-types')
-const mockStatement = require('../../mocks/mock-statement')
 const { topUpSchedule } = require('../../mocks/mock-schedule')
 
-describe('validate message body can be processed as statement', () => {
+const { validateRequest } = require('../../../app/messaging/validate-request')
+
+let statement
+
+describe('validate statement', () => {
+  beforeEach(() => {
+    statement = JSON.parse(JSON.stringify(require('../../mocks/mock-statement')))
+  })
+
   test('does not throw on valid statement', async () => {
-    expect(() => validateRequest(mockStatement, STATEMENT)).not.toThrow()
+    expect(() => validateRequest(statement, STATEMENT)).not.toThrow()
+  })
+
+  test('returns undefined on valid statement', async () => {
+    statement.documentReference = ''
+    const res = validateRequest(statement, STATEMENT)
+    expect(res).toBeUndefined()
+  })
+
+  test('does not throw on statement with empty documentReference', async () => {
+    statement.documentReference = ''
+    expect(() => validateRequest(statement, STATEMENT)).not.toThrow()
+  })
+
+  test('returns undefined on statement with empty documentReference', async () => {
+    statement.documentReference = ''
+    const res = validateRequest(statement, STATEMENT)
+    expect(res).toBeUndefined()
+  })
+
+  test('does not throw on statement with 255 character long documentReference', async () => {
+    statement.documentReference = 'a'.repeat(255)
+    expect(() => validateRequest(statement, STATEMENT)).not.toThrow()
+  })
+
+  test('returns undefined on statement with empty documentReference', async () => {
+    statement.documentReference = 'a'.repeat(255)
+    const res = validateRequest(statement, STATEMENT)
+    expect(res).toBeUndefined()
+  })
+
+  test('throws on statement with no documentReference key', async () => {
+    delete statement.documentReference
+    expect(() => validateRequest(statement, STATEMENT)).toThrow()
+  })
+
+  test('throws on statement with numerical documentReference', async () => {
+    statement.documentReference = 12345
+    expect(() => validateRequest(statement, STATEMENT)).toThrow()
+  })
+
+  test('throws on statement with true documentReference', async () => {
+    statement.documentReference = true
+    expect(() => validateRequest(statement, STATEMENT)).toThrow()
+  })
+
+  test('throws on statement with object documentReference', async () => {
+    statement.documentReference = {}
+    expect(() => validateRequest(statement, STATEMENT)).toThrow()
+  })
+
+  test('throws on statement with array documentReference', async () => {
+    statement.documentReference = []
+    expect(() => validateRequest(statement, STATEMENT)).toThrow()
+  })
+
+  test('throws on statement with null documentReference', async () => {
+    statement.documentReference = null
+    expect(() => validateRequest(statement, STATEMENT)).toThrow()
+  })
+
+  test('throws on statement with undefined documentReference', async () => {
+    statement.documentReference = undefined
+    expect(() => validateRequest(statement, STATEMENT)).toThrow()
   })
 
   test('throws on undefined statement', async () => {
@@ -49,7 +118,7 @@ describe('validate message body can be processed as statement', () => {
   })
 })
 
-describe('validate message body can be processed as schedule', () => {
+describe('validate schedule', () => {
   test('does not throw on valid schedule', async () => {
     expect(() => validateRequest(topUpSchedule, SCHEDULE)).not.toThrow()
   })
