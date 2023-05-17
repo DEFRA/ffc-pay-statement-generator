af697e8b544c33157cd491566402caa6db596c45
Marc Templeton <56440327+marctemp@users.noreply.github.com>
2023-05-17 16:42:21 +0100

    removing the lie

--- app/messaging/schemas/document-reference.js
+++ app/messaging/schemas/document-reference.js
@@ -5,6 +5,5 @@ module.exports = Joi.number().integer().positive().optional()
     'number.base': 'The document reference must be a number.',
     'number.integer': 'The document reference must be an integer.',
     'number.positive': 'The document reference must be a number greater than 0.',
-    'any.required': 'The document reference is required.',
     '*': 'The document reference must be a positive integer.'
   })
