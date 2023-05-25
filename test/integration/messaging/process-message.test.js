afb8b6e0ba989a9aad914c2517f820ddaae96ba9
Marc Templeton <marcte@kainos.com>
2023-05-25 10:31:46 +0100

    wip

--- test/integration/messaging/process-message.test.js
+++ test/integration/messaging/process-message.test.js
@@ -102,9 +102,140 @@ describe('process message', () => {
         expect(log.dateGenerated).toStrictEqual(new Date(2022, 7, 5, 15, 30, 10, 120))
       })
 
-      test('should completes message', async () => {
+      test('should send 2 messages for publish and crm', async () => {
+        await processMessage(message, receiver)
+        expect(sendMessage).toHaveBeenCalledTimes(2)
+      })
+
+      test('should send publish message with statement filename', async () => {
+        await processMessage(message, receiver)
+        expect(sendMessage.mock.calls[0][0].body.filename).toBe(FILE_NAME)
+      })
+
+      test('should send crm message with statement api link that contains filename', async () => {
+        await processMessage(message, receiver)
+        expect(sendMessage.mock.calls[1][0].body.apiLink).toContain(FILE_NAME)
+      })
+
+      test('should complete message', async () => {
         await processMessage(message, receiver)
         expect(receiver.completeMessage).toHaveBeenCalled()
+      })
+    })
+
+    describe('When statement has been processed before', () => {
+      beforeEach(async () => {
+        statement = JSON.parse(JSON.stringify(require('../../mocks/mock-statement')))
+        message = {
+          body: statement,
+          applicationProperties: {
+            type: STATEMENT.type
+          }
+        }
+
+        const { documentReference: documentRef, ...data } = statement
+
+        await db.generation.create({
+          statementData: data,
+          documentReference: documentRef,
+          filename: FILE_NAME,
+          dateGenerated: new Date(2022, 7, 5, 15, 30, 10, 120)
+        })
+      })
+
+      test('should not publish file with name FILE_NAME to archive blob storage location', async () => {
+        await processMessage(message, receiver)
+
+        const fileList = []
+        for await (const item of container.listBlobsFlat({ prefix: config.archiveFolder })) {
+          fileList.push(item.name)
+        }
+        expect(fileList).not.toContain(`${config.folder}/${FILE_NAME}`)
+      })
+
+      test('should not save another log entry', async () => {
+        await processMessage(message, receiver)
+
+        const log = await db.generation.findAll({ raw: true })
+        expect(log).toHaveLength(1)
+      })
+
+      test('should not send messages for publish and crm', async () => {
+        await processMessage(message, receiver)
+        expect(sendMessage).not.toHaveBeenCalled()
+      })
+
+      test('should complete message', async () => {
+        await processMessage(message, receiver)
+        expect(receiver.completeMessage).toHaveBeenCalled()
+      })
+    })
+
+    describe('When statement has null documentReference and nullls exist in table', () => {
+      beforeEach(async () => {
+        statement = JSON.parse(JSON.stringify(require('../../mocks/mock-statement')))
+        message = {
+          body: { ...statement, documentReference: null },
+          applicationProperties: {
+            type: STATEMENT.type
+          }
+        }
+
+        const { documentReference: _, ...data } = statement
+
+        await db.generation.create({
+          statementData: data,
+          documentReference: null,
+          filename: FILE_NAME,
+          dateGenerated: new Date(2022, 7, 5, 15, 30, 10, 120)
+        })
+      })
+
+      test('should publish file with name FILE_NAME to archive blob storage location', async () => {
+        await processMessage(message, receiver)
+
+        const fileList = []
+        for await (const item of container.listBlobsFlat({ prefix: config.archiveFolder })) {
+          fileList.push(item.name)
+        }
+        expect(fileList).toContain(`${config.folder}/${FILE_NAME}`)
+      })
+
+      test('should save another log entry', async () => {
+        await processMessage(message, receiver)
+
+        const log = await db.generation.findAll({ raw: true })
+        expect(log).toHaveLength(2)
+      })
+
+      test('should save log entry with statement data', async () => { // come back  to
+        await processMessage(message, receiver)
+
+        const log = await db.generation.findOne({ where: { filename: `${FILE_NAME}` } })
+        delete statement.documentReference
+        expect(log.statementData).toStrictEqual(statement)
+      })
+
+      test('should save log entry with statement data with no document reference', async () => {
+        await processMessage(message, receiver)
+
+        const log = await db.generation.findOne({ where: { filename: `${FILE_NAME}` } })
+        expect(Object.keys(message.body)).toContain('documentReference')
+        expect(Object.keys(log.statementData)).not.toContain('documentReference')
+      })
+
+      test('should save log entry with document reference', async () => {
+        await processMessage(message, receiver)
+
+        const log = await db.generation.findOne({ where: { filename: `${FILE_NAME}` } })
+        expect(log.documentReference).toStrictEqual(message.body.documentReference)
+      })
+
+      test('should save log entry with generation date', async () => {
+        await processMessage(message, receiver)
+
+        const log = await db.generation.findOne({ where: { filename: `${FILE_NAME}` } })
+        expect(log.dateGenerated).toStrictEqual(new Date(2022, 7, 5, 15, 30, 10, 120))
       })
 
       test('should send 2 messages for publish and crm', async () => {
@@ -120,6 +251,11 @@ describe('process message', () => {
       test('should send crm message with statement api link that contains filename', async () => {
         await processMessage(message, receiver)
         expect(sendMessage.mock.calls[1][0].body.apiLink).toContain(FILE_NAME)
+      })
+
+      test('should complete message', async () => {
+        await processMessage(message, receiver)
+        expect(receiver.completeMessage).toHaveBeenCalled()
       })
     })
   })
