8e7c44d6e830ca2b7167a48b21c3abcbb819e381
Marc Templeton <marcte@kainos.com>
2023-05-26 16:54:58 +0100

    switched to using funcs

--- app/messaging/publish/create-message.js
+++ app/messaging/publish/create-message.js
@@ -1,20 +1,8 @@
-const MESSAGE_SOURCE = require('../../constants/message-source')
+const mapPublish = require('./map-publish')
+const validatePublish = require('./validate-publish')
 
-const createMessage = (statement, filename, typeId) => {
-  return {
-    body: {
-      businessName: statement.businessName,
-      sbi: statement.sbi,
-      frn: statement.frn,
-      address: statement.address,
-      email: statement.email,
-      filename,
-      scheme: statement.scheme,
-      documentReference: statement?.documentReference ?? null
-    },
-    type: `uk.gov.pay.${typeId}.publish`,
-    source: MESSAGE_SOURCE
-  }
+const createMessage = (document, filename, type) => {
+  return validatePublish(mapPublish(document, filename, type), type)
 }
 
 module.exports = createMessage
