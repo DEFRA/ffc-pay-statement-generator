ae7bdb733ef190dcca85255decff93a51e09cfad
Marc Templeton <marcte@kainos.com>
2023-06-26 14:56:13 +0100

    configure Helm

--- app/config/notify.js
+++ app/config/notify.js
@@ -1,11 +1,13 @@
 const Joi = require('joi')
 
 const schema = Joi.object({
+  notifyEmailAddress: Joi.string().required(),
   notifyApiKey: Joi.string().required(),
   notifyEmailTemplateKey: Joi.string().required()
 })
 
 const config = {
+  notifyEmailAddress: process.env.NOTIFY_EMAIL_ADDRESS,
   notifyApiKey: process.env.NOTIFY_API_KEY,
   notifyEmailTemplateKey: process.env.NOTIFY_EMAIL_TEMPLATE_KEY
 }
