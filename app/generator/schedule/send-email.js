0442f9eb235c4699227635bab86371daa6ba0c31
Marc Templeton <56440327+marctemp@users.noreply.github.com>
2023-07-14 09:59:49 +0100

    Updating Notify template (#52)

--- app/generator/schedule/send-email.js
+++ app/generator/schedule/send-email.js
@@ -2,9 +2,14 @@ const { NotifyClient } = require('notifications-node-client')
 
 const { notifyConfig } = require('../../config')
 
-const sendEmail = async (filename) => {
+const sendEmail = async (filename, agreementNumber) => {
   const notifyClient = new NotifyClient(notifyConfig.apiKey)
-  await notifyClient.sendEmail(notifyConfig.emailTemplateKey, notifyConfig.emailAddress)
+  await notifyClient.sendEmail(notifyConfig.emailTemplateKey, notifyConfig.emailAddress, {
+    personalisation: {
+      agreementNumber,
+      filename
+    }
+  })
   console.log(`Schedule email sent for ${filename}`)
 }
 
--- test/unit/generator/schedule/send-email.test.js
+++ test/unit/generator/schedule/send-email.test.js
@@ -4,44 +4,52 @@ const { mockNotifyClient } = require('../../../mocks/modules/notifications-node-
 
 const sendEmail = require('../../../../app/generator/schedule/send-email')
 
+let filename
+let agreementNumber
+
 describe('Send email', () => {
+  beforeEach(() => {
+    filename = 'a'
+    agreementNumber = ''
+  })
+
   afterEach(() => {
     jest.clearAllMocks()
   })
 
   describe('When email is sent successfully', () => {
     test('should call mockNotifyClient', async () => {
-      await sendEmail()
+      await sendEmail(filename, agreementNumber)
       expect(mockNotifyClient).toHaveBeenCalled()
     })
 
     test('should call mockNotifyClient once', async () => {
-      await sendEmail()
+      await sendEmail(filename, agreementNumber)
       expect(mockNotifyClient).toHaveBeenCalledTimes(1)
     })
 
     test('should call mockNotifyClient with notifyConfig.apiKey', async () => {
-      await sendEmail()
+      await sendEmail(filename, agreementNumber)
       expect(mockNotifyClient).toHaveBeenCalledWith(notifyConfig.apiKey)
     })
 
     test('should call mockNotifyClient.sendEmail', async () => {
-      await sendEmail()
+      await sendEmail(filename, agreementNumber)
       expect(mockNotifyClient().sendEmail).toHaveBeenCalled()
     })
 
     test('should call mockNotifyClient.sendEmail once', async () => {
-      await sendEmail()
+      await sendEmail(filename, agreementNumber)
       expect(mockNotifyClient().sendEmail).toHaveBeenCalledTimes(1)
     })
 
-    test('should call mockNotifyClient.sendEmail with notifyConfig.emailTemplateKey and notifyConfig.emailAddress', async () => {
-      await sendEmail()
-      expect(mockNotifyClient().sendEmail).toHaveBeenCalledWith(notifyConfig.emailTemplateKey, notifyConfig.emailAddress)
+    test('should call mockNotifyClient.sendEmail with notifyConfig.emailTemplateKey, notifyConfig.emailAddress and { personalisation: { agreementNumber: agreementNumber, filename: filename } }', async () => {
+      await sendEmail(filename, agreementNumber)
+      expect(mockNotifyClient().sendEmail).toHaveBeenCalledWith(notifyConfig.emailTemplateKey, notifyConfig.emailAddress, { personalisation: { agreementNumber, filename } })
     })
 
     test('should return undefined', async () => {
-      const result = await sendEmail()
+      const result = await sendEmail(filename, agreementNumber)
       expect(result).toBeUndefined()
     })
   })
@@ -53,47 +61,47 @@ describe('Send email', () => {
     })
 
     test('should call mockNotifyClient', async () => {
-      try { await sendEmail() } catch {}
+      try { await sendEmail(filename, agreementNumber) } catch {}
       expect(mockNotifyClient).toHaveBeenCalled()
     })
 
     test('should call mockNotifyClient once', async () => {
-      try { await sendEmail() } catch {}
+      try { await sendEmail(filename, agreementNumber) } catch {}
       expect(mockNotifyClient).toHaveBeenCalledTimes(1)
     })
 
     test('should call mockNotifyClient with notifyConfig.apiKey', async () => {
-      try { await sendEmail() } catch {}
+      try { await sendEmail(filename, agreementNumber) } catch {}
       expect(mockNotifyClient).toHaveBeenCalledWith(notifyConfig.apiKey)
     })
 
     test('should call mockNotifyClient.sendEmail', async () => {
-      try { await sendEmail() } catch {}
+      try { await sendEmail(filename, agreementNumber) } catch {}
       expect(mockNotifyClient().sendEmail).toHaveBeenCalled()
     })
 
     test('should call mockNotifyClient.sendEmail once', async () => {
-      try { await sendEmail() } catch {}
+      try { await sendEmail(filename, agreementNumber) } catch {}
       expect(mockNotifyClient().sendEmail).toHaveBeenCalledTimes(1)
     })
 
-    test('should call mockNotifyClient.sendEmail with notifyConfig.emailTemplateKey and notifyConfig.emailAddress', async () => {
-      try { await sendEmail() } catch {}
-      expect(mockNotifyClient().sendEmail).toHaveBeenCalledWith(notifyConfig.emailTemplateKey, notifyConfig.emailAddress)
+    test('should call mockNotifyClient.sendEmail with notifyConfig.emailTemplateKey, notifyConfig.emailAddress and { personalisation: { agreementNumber: agreementNumber, filename: filename } }', async () => {
+      try { await sendEmail(filename, agreementNumber) } catch {}
+      expect(mockNotifyClient().sendEmail).toHaveBeenCalledWith(notifyConfig.emailTemplateKey, notifyConfig.emailAddress, { personalisation: { agreementNumber, filename } })
     })
 
     test('should throw', async () => {
-      const wrapper = async () => { await sendEmail() }
+      const wrapper = async () => { await sendEmail(filename, agreementNumber) }
       expect(wrapper).rejects.toThrow()
     })
 
     test('should throw Error', async () => {
-      const wrapper = async () => { await sendEmail() }
+      const wrapper = async () => { await sendEmail(filename, agreementNumber) }
       expect(wrapper).rejects.toThrow(Error)
     })
 
     test('should throw error "Notify issue"', async () => {
-      const wrapper = async () => { await sendEmail() }
+      const wrapper = async () => { await sendEmail(filename, agreementNumber) }
       expect(wrapper).rejects.toThrow(/^Notify issue$/)
     })
   })
