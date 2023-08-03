8d4f97143a3ae6ddfd4ee718023e0eb507c6bc88
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2023-08-03 09:24:32 +0100

    removed commenting

--- app/generator/content/statement/index.js
+++ app/generator/content/statement/index.js
@@ -1,7 +1,6 @@
 const summary = require('./summary');
 const part1 = require('./part1');
 const part2 = require('./part2');
-// const part3 = require('./part3') remove when checked working
 const getHelpInfo = require('../common/part3/get-help-info');
 
 const createContent = (statement) => {
--- app/generator/styles.js
+++ app/generator/styles.js
@@ -1,57 +1,57 @@
-const { millimetresToPoints } = require('./conversion')
+const { millimetresToPoints } = require('./conversion');
 
 module.exports = {
-  default: {
-    font: 'Arial',
-    fontSize: 12,
-    lineHeight: 1.15
-  },
-  header1: {
-    fontSize: 24,
-    bold: true,
-    margin: [0, 10, 0, 10]
-  },
-  header2: {
-    fontSize: 16,
-    bold: true,
-    margin: [0, 40, 0, 10]
-  },
-  header3: {
-    fontSize: 12,
-    bold: true,
-    margin: [0, 10, 0, 10]
-  },
-  subTitle: {
-    fontSize: 18,
-    margin: [0, 0, 0, 30]
-  },
-  link: {
-    decoration: 'underline',
-    color: 'blue'
-  },
-  table: {
-    fontSize: 10,
-    margin: [0, 10, 0, 10]
-  },
-  tableHeader: {
-    fontSize: 12,
-    bold: true,
-    margin: [0, 0, 0, 5]
-  },
-  tableNumber: {
-    alignment: 'right'
-  },
-  logo: {
-    margin: [0, 20, 0, millimetresToPoints(50)]
-  },
-  address: {
-    fontSize: 9.5,
-    lineHeight: 1
-  },
-  scheduleLogo: {
-    margin: [400, 20, 0, millimetresToPoints(50)]
-  },
-  style: {
-    margin: [0, 0, 0, 5]
-  }
-}
+	default: {
+		font: 'Arial',
+		fontSize: 12,
+		lineHeight: 1.15
+	},
+	header1: {
+		fontSize: 24,
+		bold: true,
+		margin: [0, 10, 0, 10]
+	},
+	header2: {
+		fontSize: 16,
+		bold: true,
+		margin: [0, 40, 0, 10]
+	},
+	header3: {
+		fontSize: 12,
+		bold: true,
+		margin: [0, 10, 0, 10]
+	},
+	subTitle: {
+		fontSize: 18,
+		margin: [0, 0, 0, 30]
+	},
+	link: {
+		decoration: 'underline',
+		color: 'blue'
+	},
+	table: {
+		fontSize: 10,
+		margin: [0, 10, 0, 10]
+	},
+	tableHeader: {
+		fontSize: 12,
+		bold: true,
+		margin: [0, 0, 0, 5]
+	},
+	tableNumber: {
+		alignment: 'right'
+	},
+	logo: {
+		margin: [0, 20, 0, millimetresToPoints(50)]
+	},
+	address: {
+		fontSize: 9.5,
+		lineHeight: 1
+	},
+	scheduleLogo: {
+		margin: [400, 20, 0, millimetresToPoints(50)]
+	},
+	style: {
+		margin: [0, 0, 0, 5]
+	}
+};
