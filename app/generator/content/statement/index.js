9613d93dfee5393d1010812993e4033b5999b975
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2023-08-03 09:09:11 +0100

    fixed location for common

--- app/generator/content/statement/index.js
+++ app/generator/content/statement/index.js
@@ -1,17 +1,18 @@
-const summary = require('./summary')
-const part1 = require('./part1')
-const part2 = require('./part2')
-const part3 = require('./part3')
+const summary = require('./summary');
+const part1 = require('./part1');
+const part2 = require('./part2');
+// const part3 = require('./part3') remove when checked working
+const getHelpInfo = require('../common/part3/get-help-info');
 
 const createContent = (statement) => {
-  return [
-    summary(statement),
-    part1(statement),
-    part2(statement),
-    part3()
-  ]
-}
+	return [
+		summary(statement),
+		part1(statement),
+		part2(statement),
+		getHelpInfo()
+	];
+};
 
 module.exports = {
-  createContent
-}
+	createContent
+};
