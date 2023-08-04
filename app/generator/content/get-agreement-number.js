f63fc73a3eb8cb22c74f56446bfbff773c371e1b
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2023-08-04 13:48:11 +0100

    edited paths pointed to the wrong area since refactoring

---
+++ app/generator/content/get-agreement-number.js
@@ -0,0 +1,12 @@
+const getAgreementNumber = (agreementNumber) => {
+	return {
+		columns: [
+			{ width: 200, text: 'Agreement number:' },
+			{ width: '*', text: agreementNumber }
+		],
+		style: 'column',
+		columnGap: 10
+	};
+};
+
+module.exports = getAgreementNumber;
---
+++ app/generator/content/get-summary.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const { millimetresToPoints } = require('../conversion')
+const getAddress = require('./get-address')
+const getSBI = require('./get-sbi')
+const getBusinessName = require('./get-business-name')
+const getAgreementNumber = require('./get-agreement-number')
+const imagePath = path.join(__dirname, '..', 'images')
+
+const getSummary = (summary) => {
+	return {
+		stack: [
+			{
+				image: `${imagePath}/v2/logo.jpg`,
+				fit: [millimetresToPoints(200), millimetresToPoints(25)],
+				style: 'logo'
+			},
+			getAddress(summary.businessName, summary.address),
+			{
+				text: `${summary.scheme.name} ${summary.scheme.year}`,
+				style: 'header14'
+			},
+			{ text: 'Payment summary', style: 'header14' },
+			getSBI(summary.sbi),
+			getBusinessName(summary.businessName),
+			getAgreementNumber(summary.scheme.agreementNumber),
+		],
+		unbreakable: true
+	};
+};
+
+module.exports = getSummary;
