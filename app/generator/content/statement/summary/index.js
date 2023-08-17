423b5b2623942c9cae9f9c0eab734c72e0e8aeef
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2023-08-17 14:44:48 +0100

    removing an additional line break

--- app/generator/content/statement/summary/index.js
+++ app/generator/content/statement/summary/index.js
@@ -4,7 +4,7 @@ const summary = (statement) => {
   return {
     stack: [
       commonHeader(),
-      `\nThis statement explains your payment for the ${statement.scheme.name} (${statement.scheme.shortName}). It is made up of 3 parts.`,
+      `This statement explains your payment for the ${statement.scheme.name} (${statement.scheme.shortName}). It is made up of 3 parts.`,
       '\nPart 1 provides a summary of the most recent payment.',
       'Part 2 explains how we calculated the payment.',
       'Part 3 highlights where to go for more support.'
