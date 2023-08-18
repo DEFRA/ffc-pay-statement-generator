f2a48485d2b5635ff00ee12b2adb83302a79f3e6
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2023-08-18 21:22:46 +0100

    get help info, removed SFI section, added part3. text

--- app/generator/content/get-help-info.js
+++ app/generator/content/get-help-info.js
@@ -1,7 +1,7 @@
 const getHelpInfo = () => {
   return {
     stack: [
-      { text: 'More information', style: 'h3' },
+      { text: 'Part 3. More information', style: 'h3' },
       'If you think your payments are wrong, you should:\n',
       {
         ul: [{
@@ -26,16 +26,6 @@ const getHelpInfo = () => {
           'You can email ',
           { text: 'ruralpayments@defra.gov.uk', link: 'mailto:ruralpayments@defra.gov.uk', decoration: 'underline' },
           ' or call us on 03000 200 301 (Monday to Friday, 8.30am to 5pm). Please tell us your SBI and Agreement number if applicable which are at the top of this email.'
-        ]
-      },
-      { text: 'SFI', style: 'h3' },
-      {
-        text: [
-          'Rural Payments Agency | PO Box 325 | WORKSOP | S95 1DG',
-          { text: '\nruralpayments@defra.gov.uk', link: 'mailto:ruralpayments@defra.gov.uk', decoration: 'underline' },
-          ' | 03000 200 301 | ',
-          { text: 'gov.uk/defra/sfi', link: 'https://www.gov.uk/government/collections/sustainable-farming-incentive-guidance', decoration: 'underline' },
-          '\nFollow us on Twitter @Ruralpay'
         ]
       }
     ],
