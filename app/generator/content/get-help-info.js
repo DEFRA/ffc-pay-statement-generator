28b1474c4c45ce59b4304abfe0d8526585f2de86
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2023-08-18 21:14:30 +0100

    scheme name and year missing correct styling

---
+++ app/generator/content/get-help-info.js
@@ -0,0 +1,46 @@
+const getHelpInfo = () => {
+  return {
+    stack: [
+      { text: 'More information', style: 'h3' },
+      'If you think your payments are wrong, you should:\n',
+      {
+        ul: [{
+          text: [
+            'log into the Rural Payments service ',
+            { text: 'www.gov.uk/claim-rural-payments', link: 'https://www.gov.uk/claim-rural-payments', decoration: 'underline' },
+            ' to check your SFI standard agreement'
+          ]
+        },
+        'check statements and letters you\'ve received from us',
+        {
+          text: [
+            'review the SFI scheme information at ',
+            { text: 'www.gov.uk/government/collections/sustainable-farming-incentive-guidance', link: 'https://www.gov.uk/government/collections/sustainable-farming-incentive-guidance', decoration: 'underline' }
+          ]
+        }],
+        listStyle: 'square'
+      },
+      { text: 'If you have any questions about this email', style: 'h3' },
+      {
+        text: [
+          'You can email ',
+          { text: 'ruralpayments@defra.gov.uk', link: 'mailto:ruralpayments@defra.gov.uk', decoration: 'underline' },
+          ' or call us on 03000 200 301 (Monday to Friday, 8.30am to 5pm). Please tell us your SBI and Agreement number if applicable which are at the top of this email.'
+        ]
+      },
+      { text: 'SFI', style: 'h3' },
+      {
+        text: [
+          'Rural Payments Agency | PO Box 325 | WORKSOP | S95 1DG',
+          { text: '\nruralpayments@defra.gov.uk', link: 'mailto:ruralpayments@defra.gov.uk', decoration: 'underline' },
+          ' | 03000 200 301 | ',
+          { text: 'gov.uk/defra/sfi', link: 'https://www.gov.uk/government/collections/sustainable-farming-incentive-guidance', decoration: 'underline' },
+          '\nFollow us on Twitter @Ruralpay'
+        ]
+      }
+    ],
+    unbreakable: true
+  }
+}
+
+module.exports = getHelpInfo
--- app/generator/content/statement/summary/index.js
+++ app/generator/content/statement/summary/index.js
@@ -1,9 +1,8 @@
 const summary = (statement) => {
   return {
     stack: [
-      { text: `${statement.scheme.name} ${statement.scheme.year}`, style: 'header2' },
-      { text: 'Payment statement', style: 'subTitle' },
-      `\nThis statement explains your payment for the ${statement.scheme.name} (${statement.scheme.shortName}). It is made up of 3 parts.`,
+      { text: `${statement.scheme.name} ${statement.scheme.year} Payment statement`, style: 'h2' },
+      `This statement explains your payment for the ${statement.scheme.name} (${statement.scheme.shortName}). It is made up of 3 parts.`,
       '\nPart 1 provides a summary of the most recent payment.',
       'Part 2 explains how we calculated the payment.',
       'Part 3 highlights where to go for more support.'
