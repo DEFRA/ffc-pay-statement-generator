bebe89fd38952a6c3a7c7e30dbf03adc289ffed1
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2023-11-02 16:03:43 +0000

    updated and improved unit tests

--- test/unit/generator/content/statement/SFIA/content/data-protection.test.js
+++ test/unit/generator/content/statement/SFIA/content/data-protection.test.js
@@ -1,7 +1,7 @@
 const dataProtection = require('../../../../../../../app/generator/content/data-protection')
 
 describe('dataProtection', () => {
-  it('should return the correct data protection object', () => {
+  test('should return the correct data protection object', () => {
     const result = dataProtection()
 
     expect(result).toHaveProperty('style', 'govuk-body-s')
@@ -11,13 +11,13 @@ describe('dataProtection', () => {
     expect(result.stack[2]).toHaveProperty('text', '\'Rural Payments Agency personal information charter.\'')
     expect(result.stack[2]).toHaveProperty('link', 'https://www.gov.uk/claim-rural-payments')
     expect(result.stack[2]).toHaveProperty('decoration', 'underline')
-    expect(result.stack[3]).toHaveProperty('text', 'About this email')
-    expect(result.stack[4]).toHaveProperty('text', 'This email has been sent to you because the email address has been provided to us. If you no longer wish to be contacted electronically, please let us know. If you have done this recently please allow for this to take effect.')
-    expect(result.stack[5]).toHaveProperty('text', 'Security')
-    expect(result.stack[6]).toHaveProperty('text', 'Please be on your guard against emails that request any of your security details. We will never ask you by email to enter (or record) these details. If you receive an email like this, you should not respond.')
-    expect(result.stack[7]).toHaveProperty('text', 'Legal')
-    expect(result.stack[8]).toHaveProperty('text', 'This email message is confidential and for use by the addressee only. If the message is received by anyone other than the addressee, please delete it from your computer.')
-    expect(result.stack[9]).toHaveProperty('text', 'The Rural Payments Agency does not accept responsibility for changes made to this message after it was sent.')
-    expect(result.stack[10]).toHaveProperty('text', 'While all reasonable care has been taken to avoid the transmission of viruses, it is your responsibility to ensure that onward transmission, opening or use of this message and any attachments will not adversely affect your systems or data. No responsibility is accepted by the Rural Payments Agency in this regard and you should carry out appropriate virus and other checks.')
+    expect(result.stack[4]).toHaveProperty('text', 'About this email')
+    expect(result.stack[5]).toBe('This email has been sent to you because the email address has been provided to us. If you no longer wish to be contacted electronically, please let us know. If you have done this recently please allow for this to take effect.')
+    expect(result.stack[7]).toHaveProperty('text', 'Security')
+    expect(result.stack[8]).toBe('Please be on your guard against emails that request any of your security details. We will never ask you by email to enter (or record) these details. If you receive an email like this, you should not respond.')
+    expect(result.stack[10]).toHaveProperty('text', 'Legal')
+    expect(result.stack[11]).toBe('This email message is confidential and for use by the addressee only. If the message is received by anyone other than the addressee, please delete it from your computer.')
+    expect(result.stack[13]).toHaveProperty('text', 'The Rural Payments Agency does not accept responsibility for changes made to this message after it was sent.')
+    expect(result.stack[15]).toBe('While all reasonable care has been taken to avoid the transmission of viruses, it is your responsibility to ensure that onward transmission, opening or use of this message and any attachments will not adversely affect your systems or data. No responsibility is accepted by the Rural Payments Agency in this regard and you should carry out appropriate virus and other checks.')
   })
 })
--- test/unit/generator/content/statement/SFIA/content/summary.test.js
+++ test/unit/generator/content/statement/SFIA/content/summary.test.js
@@ -9,15 +9,12 @@ describe('advanceSummary', () => {
     }
   }
 
-  it('should return the correct summary object', () => {
+  test('should return the correct summary object', () => {
     const result = advanceSummary(document)
 
-    expect(result.stack).toHaveLength(6)
+    expect(result.stack).toHaveLength(7)
     expect(result.stack[0]).toHaveProperty('image')
-    expect(result.stack[1]).toHaveProperty('text', 'SBI: 12345678')
-    expect(result.stack[2]).toHaveProperty('text', 'Business Name: Example Business')
-    expect(result.stack[3]).toHaveProperty('text', 'Agreement Number: ABC123')
     expect(result.stack[4]).toHaveProperty('text', 'Our ref: SFI 23 Accelerated payment v1.0')
-    expect(result.stack[5]).toHaveProperty('text', 'Dear Sir / Madam')
+    expect(result.stack[6]).toHaveProperty('text', 'Dear Sir / Madam')
   })
 })
