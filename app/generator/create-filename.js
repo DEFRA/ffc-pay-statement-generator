34b1d6994962aed9904cc4c69825cf3a57789ffc
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-01-23 14:26:53 +0000

    refactor create-filename to verify regex pattern before filename is created or throw error

--- app/generator/create-filename.js
+++ app/generator/create-filename.js
@@ -2,9 +2,14 @@ const { STATEMENT, SCHEDULE } = require('../constants/document-types')
 
 const createFilename = (statement, timestamp, type) => {
   const prefix = getPrefix(type)
-  return `FFC_Payment${prefix}_${statement.scheme.shortName}_${statement.scheme.year}_${statement.frn}_${timestamp}.pdf`.replace(/\s/g, '')
+  const filename = `FFC_Payment${prefix}_${statement.scheme.shortName}_${statement.scheme.year}_${statement.frn}_${timestamp}.pdf`.replace(/\s/g, '')
+  const filenameIsValid = /^[A-Z]{3,6}_[A-Z]([A-Z0-9]*[a-z][a-z0-9]*[A-Z]|[a-z0-9]*[A-Z][A-Z0-9]*[a-z])[A-Za-z0-9]*_[A-Z]{3,6}_\d{4}_\d{10}_\d{16}\.pdf$/
+  if (filenameIsValid.test(filename)) {
+    return filename
+  } else if (!filenameIsValid.test(filename)) {
+    throw new Error('Filename does not meet the correct format')
+  }
 }
-
 const getPrefix = (type) => {
   switch (type) {
     case STATEMENT:
