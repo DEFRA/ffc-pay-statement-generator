4e5e8525c1aaa83151d6553ca96ba7a49bfb51f4
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-02-01 14:03:19 +0000

    changed errorStatement to mockStatement

--- test/unit/generator/validate-filename.test.js
+++ test/unit/generator/validate-filename.test.js
@@ -1,45 +1,37 @@
 const moment = require('moment')
 const getFilename = require('../../../app/generator/create-filename')
 const { STATEMENT } = require('../../../app/constants/document-types')
+const mockStatement = require('../../mocks/mock-statement')
 
 let timestamp
-let errorStatement
 
 describe('Validation functions', () => {
   beforeEach(() => {
     jest.useFakeTimers().setSystemTime(new Date(2022, 7, 5, 15, 30, 10, 120))
     timestamp = moment(new Date()).format('YYYYMMDDHHmmssSS')
-    errorStatement = require('../../mocks/error-statement')
+    mockStatement.scheme.shortName = 'SFI'
+    mockStatement.scheme.year = '2022'
+    mockStatement.frn = 1234567890
+    // mockStatement.scheme.shortName = 'SFI'
   })
 
   test('checks error message on incorrect scheme', () => {
-    errorStatement.scheme.year = '2022'
-    errorStatement.frn = 1234567890
-    timestamp = '2022080515301012'
-    expect(() => getFilename(errorStatement, timestamp, STATEMENT)).toThrow('invalid short name')
+    mockStatement.scheme.shortName = 'invalid'
+    expect(() => getFilename(mockStatement, timestamp, STATEMENT)).toThrow('invalid short name')
   })
 
   test('checks error message on incorrect scheme year', () => {
-    errorStatement.scheme.shortName = 'SFI'
-    errorStatement.scheme.year = '20220'
-    errorStatement.frn = 1234567890
-    timestamp = '2022080515301012'
-    expect(() => getFilename(errorStatement, timestamp, STATEMENT)).toThrow('invalid scheme year')
+    mockStatement.scheme.year = '20220'
+    expect(() => getFilename(mockStatement, timestamp, STATEMENT)).toThrow('invalid scheme year')
   })
 
   test('checks error message on incorrect frn', () => {
-    errorStatement.scheme.shortName = 'SFI'
-    errorStatement.scheme.year = '2022'
-    errorStatement.frn = '123456789012'
-    timestamp = '2022080515301012'
-    expect(() => getFilename(errorStatement, timestamp, STATEMENT)).toThrow('invalid frn number')
+    mockStatement.frn = '123456789012'
+    expect(() => getFilename(mockStatement, timestamp, STATEMENT)).toThrow('invalid frn number')
   })
 
   test('checks error message on incorrect timestamp', () => {
-    errorStatement.scheme.shortName = 'SFI'
-    errorStatement.scheme.year = '2022'
-    errorStatement.frn = 1234567890
     timestamp = '202208051530'
-    expect(() => getFilename(errorStatement, timestamp, STATEMENT)).toThrow('invalid timestamp')
+    expect(() => getFilename(mockStatement, timestamp, STATEMENT)).toThrow('invalid timestamp')
   })
 })
